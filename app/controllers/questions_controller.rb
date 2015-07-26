e359daf15138fd02c42fa3e5b7911643d6738b85
Richard Lamb <pair+richard@gmail.com>
2015-07-26 12:13:46 +0100

    Add ability to edit questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_question, except: [:new, :create]
 
   def new
     @question = Question.new
@@ -15,13 +16,26 @@ class QuestionsController < ApplicationController
   end
 
   def show
-    @question = Question.find(params[:id])
     @answer   = Answer.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @question.update(question_params)
+      flash[:success] = 'Your question was modified successfully'
+      redirect_to question_path(@question)
+    end
   end
 
   private
 
   def question_params
     params.require(:question).permit(:title, :text)
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 end
--- spec/features/questions_spec.rb
+++ spec/features/questions_spec.rb
@@ -2,19 +2,34 @@ require 'rails_helper'
 
 describe 'Questions', :js do
 
-  let(:user) { create(:user) }
+  let(:user)           { create(:user) }
+  let(:question_title) { Faker::Lorem.sentence }
+
+  before(:each) do
+    login(user)
+  end
 
   scenario 'adding a question' do
-    login(user)
-
-    question_title = Faker::Lorem.sentence
-
     fill_in :question_title, with: question_title
     fill_in :question_text, with: Faker::Lorem.paragraph
-    click_on 'Submit Question'
+    click_on 'Create Question'
 
     expect(page).to have_content 'Your question was submitted successfully'
     expect(page).to have_content question_title
   end
 
+  context 'existing question' do
+    let(:question) { create(:question) }
+
+    scenario 'editing a question' do
+      visit question_path(question)
+      click_on 'Edit Question'
+
+      fill_in  :question_title, with: question_title
+      click_on 'Update Question'
+
+      expect(page).to have_content 'Your question was modified successfully'
+      expect(page).to have_content question_title
+    end
+  end
 end
